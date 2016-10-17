b1285e5819b48907dffc69234916a0ba96f63ba4
Jill Kuchman <jillkuchman@gmail.com>
2016-10-16 18:14:08 -0700

    Add task class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,18 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'my-app',
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
+  </div>
   `
 })
 
 export class AppComponent {
+}
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number){}
 }
